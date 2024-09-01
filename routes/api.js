fd58fac064f3a7e6cfdc6d7b837166d6bee48f95
al-script <alec.sorensen.as@gmail.com>
2024-09-01 14:34:51 -0700

    Merge project files

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,162 @@
-'use strict';
+"use strict";
 
 module.exports = function (app) {
+  require("dotenv").config();
+  const bcrypt = require("bcrypt");
+  const fetch = (...args) =>
+    import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+  const mongoose = require("mongoose");
+  mongoose
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .catch((e) => {
+      console.log(e.message);
+      process.exit(1);
+    })
+    .then(() => {
+      console.log("Connected to Mongo Atlas");
     });
-    
+
+  let stockSchema = new mongoose.Schema({
+    symbol: { type: String, require: true },
+    likes: { type: Number },
+    IP: { type: [String] },
+  });
+
+  let Stock = mongoose.model("Stock", stockSchema);
+
+  async function asyncStockDbHandler(stockSymbol, like, userIP, secureIP) {
+    let foundStock = await Stock.findOne({ symbol: stockSymbol });
+    if (foundStock === null) {
+      let newStock = new Stock({
+        symbol: stockSymbol,
+        likes: 0,
+        IP: [""],
+      });
+      foundStock = await newStock.save();
+    }
+    if (like === "true") {
+      // if want to test a boolean then will have to refactor the like variable as a boolean
+      // console.log("Stock was liked, checking for IP in database");
+      let wasIPFoundInDb;
+      foundStock.IP.forEach((hash) => {
+        // could refactor as a for-loop so can break once find an IP
+        let foundUserIPInDb = bcrypt.compareSync(userIP, hash);
+        if (foundUserIPInDb === true) {
+          wasIPFoundInDb = true;
+        }
+      });
+      // console.log("Was IP found in database?", wasIPFoundInDb);
+      if (!wasIPFoundInDb) {
+        // console.log("IP was not found in database");
+        // console.log("Previous likes:", foundStock.likes);
+        foundStock.likes++;
+        foundStock.IP.push(secureIP);
+        // console.log("Saving...");
+        return await foundStock.save();
+      } else {
+        // console.log("IP was found in the database");
+        return foundStock;
+      }
+    } else {
+      // console.log("Stock was not liked");
+      return foundStock; // could refactor so just have one return foundStock
+    }
+  }
+
+  async function getStockApiData(apiURL) {
+    let response = await fetch(apiURL);
+    return response.json();
+  }
+
+  async function stockPriceHandler(req, res) {
+    try {
+      // Define stock symbol data from user request
+      // console.log("Initialize stock symbol data");
+      let stockSymbol1, stockSymbol2, apiURL;
+      if (Array.isArray(req.query.stock)) {
+        stockSymbol1 = req.query.stock[0];
+        stockSymbol2 = req.query.stock[1];
+        apiURL = `https://stock-price-checker.freecodecamp.rocks/api/stock-prices?stock=${stockSymbol1}&stock=${stockSymbol2}`;
+      } else {
+        stockSymbol1 = req.query.stock;
+        apiURL = `https://stock-price-checker.freecodecamp.rocks/api/stock-prices?stock=${stockSymbol1}`;
+      }
+
+      // Fetch stock data from API using the previously generated API URL
+      // await time(1000);
+      // console.log("Fetch stock data from API");
+      let apiData = await getStockApiData(apiURL);
+
+      // console.log("+++++, Stock.findOne( {symbol: stockSymbol} )", Stock.findOne( {symbol: stockSymbol1} ))
+
+      // Define price data
+      // await time(2000);
+      // console.log("Define price data");
+      let price1, price2;
+      if (stockSymbol2) {
+        price1 = apiData.stockData[0].price;
+        price2 = apiData.stockData[1].price;
+      } else {
+        price1 = apiData.stockData.price;
+      }
+
+      // console.log(price1);
+
+      // Generate secure version of IP (perhaps refactor so only genereate if user liked the stock...)
+      // await time(1000);
+      // console.log("Generate secure version of IP if user liked the stock");
+      // console.log("Liked?", req.query.like);
+      let like = req.query.like; // stored as a string... refactor to return a boolean so that in the async db function can test simply for boolean and not a string
+      let userIP = req.socket.remoteAddress;
+      let secureIP = bcrypt.hashSync(userIP, 12);
+
+      // handle database operations, and create object containing stock data
+      // await time(1000);
+      // console.log(
+      //   "handle database operations, and create object containing stock data"
+      // );
+      let stockDataToReturn;
+      let stock1 = await asyncStockDbHandler(
+        stockSymbol1,
+        like,
+        userIP,
+        secureIP
+      );
+      if (stockSymbol2) {
+        let stock2 = await asyncStockDbHandler(
+          stockSymbol2,
+          like,
+          userIP,
+          secureIP
+        );
+        let rel_likes = stock1.likes - stock2.likes;
+        stockDataToReturn = [
+          { stock: stockSymbol1, price: +price1, rel_likes: +rel_likes },
+          { stock: stockSymbol2, price: +price2, rel_likes: +rel_likes * -1 },
+        ];
+      } else {
+        stockDataToReturn = {
+          stock: stockSymbol1,
+          price: +price1,
+          likes: +stock1.likes,
+        };
+      }
+
+      // await time(1000);
+      // console.log("Return the final object");
+      // res.json({ stockData: stockDataToReturn });
+      return { "stockData": stockDataToReturn }
+    } catch (error) {
+      console.log("Error:", error);
+    } finally {
+      // console.log("Fin");
+    }
+  }
+
+  app.route("/api/stock-prices").get(async function (req, res) {
+    // console.log("Requested stock:", req.query);
+    let result = await stockPriceHandler(req, res);
+    res.json(result);
+  });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,237 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 const assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function () {
+  let likesFirst, likesSecond;
+  
+  // Viewing one stock: GET request to /api/stock-prices/
+  test("Viewing one stock: GET request to /api/stock-prices/", function (done) { // had to pass in and then call done... perhaps need to do this on my other projects as well
+    this.timeout(10000);
+    chai
+      .request(server)
+      .get("/api/stock-prices?stock=GOOG")
+      .end(function (err, res) {
+        // setTimeout(()=>{console.log("waiting...")}, 4000);
+        // console.log(res.body);
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.isObject(res.body, "response should be an object");
+        assert.property(
+          res.body,
+          "stockData",
+          "response should contain a stockData object"
+        );
+        assert.property(
+          res.body.stockData,
+          "stock",
+          "stockData object should contain stock property"
+        );
+        assert.property(
+          res.body.stockData,
+          "price",
+          "stockData object should contain price property"
+        );
+        assert.property(
+          res.body.stockData,
+          "likes",
+          "stockData object should contain likes property"
+        );
+        done();
+        // setTimeout(()=>{console.log("waiting...")}, 4000);
+      }).timeout(5000);
+  })
 
+  // Viewing one stock and liking it: GET request to /api/stock-prices/
+  test("Viewing one stock and liking it: GET request to /api/stock-prices/", function (done) {
+    this.timeout(10000);
+    chai
+      .request(server)
+      .get("/api/stock-prices?stock=GOOG&like=true")
+      .end(function (err, res) {
+        // setTimeout(()=>{console.log("waiting...")}, 4000);
+        // console.log(res.body);
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.isObject(res.body, "response should be an object");
+        assert.property(
+          res.body,
+          "stockData",
+          "response should contain a stockData object"
+        );
+        assert.property(
+          res.body.stockData,
+          "stock",
+          "stockData object should contain stock property"
+        );
+        assert.property(
+          res.body.stockData,
+          "price",
+          "stockData object should contain price property"
+        );
+        assert.property(
+          res.body.stockData,
+          "likes",
+          "stockData object should contain likes property"
+        );
+        likesFirst = res.body.likes;
+        done();
+        // assert.equal(likesAfter, likesBefore + 1, "likes should be incremented by one") //but this may break if run the test from the same IP... so perhaps don't need this test
+        // setTimeout(()=>{console.log("waiting...")}, 4000);
+      }).timeout(5000);
+  })
+
+  // Viewing the same stock and liking it again: GET request to /api/stock-prices/
+  test("Viewing the same stock and liking it again: GET request to /api/stock-prices/", function (done) {
+    this.timeout(10000);
+    chai
+      .request(server)
+      .get("/api/stock-prices?stock=GOOG&like=true")
+      .end(function (err, res) {
+        // console.log(res.body)
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.isObject(res.body, "response should be an object");
+        assert.property(
+          res.body,
+          "stockData",
+          "response should contain a stockData object"
+        );
+        assert.property(
+          res.body.stockData,
+          "stock",
+          "stockData object should contain stock property"
+        );
+        assert.property(
+          res.body.stockData,
+          "price",
+          "stockData object should contain price property"
+        );
+        assert.property(
+          res.body.stockData,
+          "likes",
+          "stockData object should contain likes property"
+        );
+        likesSecond = res.body.likes;
+        assert.equal(
+          likesFirst,
+          likesSecond,
+          "likes should not be incremented if same stock is liked"
+        );
+        done();
+      });
+  });
+
+  // Viewing two stocks: GET request to /api/stock-prices/
+  test("Viewing two stocks", function (done) {
+    this.timeout(10000);
+    chai
+      .request(server)
+      .get("/api/stock-prices?stock=GOOG&stock=MSFT")
+      .end(function (err, res) {
+        // console.log(res.body)
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.isObject(res.body, "response should be an object");
+        assert.property(
+          res.body,
+          "stockData",
+          "response should contain a stockData array"
+        );
+        assert.equal(
+          res.body.stockData.length,
+          2,
+          "stockData array should contain two objects"
+        );
+        assert.property(
+          res.body.stockData[0],
+          "stock",
+          "the first object in stockData array should contain stock property"
+        );
+        assert.property(
+          res.body.stockData[0],
+          "price",
+          "the first object in stockData should contain price property"
+        );
+        assert.property(
+          res.body.stockData[0],
+          "rel_likes",
+          "the first object in stockData should contain rel_likes property"
+        );
+        assert.property(
+          res.body.stockData[1],
+          "stock",
+          "the second object in stockData array should contain stock property"
+        );
+        assert.property(
+          res.body.stockData[1],
+          "price",
+          "the second object in stockData should contain price property"
+        );
+        assert.property(
+          res.body.stockData[1],
+          "rel_likes",
+          "the second object in stockData should contain rel_likes property"
+        );
+        done();
+      });
+  });
+
+  // Viewing two stocks and liking them: GET request to /api/stock-prices/
+  test("Viewing two stocks and liking them", function (done) {
+    this.timeout(10000);
+    chai
+      .request(server)
+      .get("/api/stock-prices?stock=GOOG&stock=MSFT&like=true")
+      .end(function (err, res) {
+        // console.log(res.body)
+        assert.equal(res.status, 200);
+        assert.equal(res.type, "application/json");
+        assert.isObject(res.body, "response should be an object");
+        assert.property(
+          res.body,
+          "stockData",
+          "response should contain a stockData array"
+        );
+        assert.equal(
+          res.body.stockData.length,
+          2,
+          "stockData array should contain two objects"
+        );
+        assert.property(
+          res.body.stockData[0],
+          "stock",
+          "the first object in stockData array should contain stock property"
+        );
+        assert.property(
+          res.body.stockData[0],
+          "price",
+          "the first object in stockData should contain price property"
+        );
+        assert.property(
+          res.body.stockData[0],
+          "rel_likes",
+          "the first object in stockData should contain rel_likes property"
+        );
+        assert.property(
+          res.body.stockData[1],
+          "stock",
+          "the second object in stockData array should contain stock property"
+        );
+        assert.property(
+          res.body.stockData[1],
+          "price",
+          "the second object in stockData should contain price property"
+        );
+        assert.property(
+          res.body.stockData[1],
+          "rel_likes",
+          "the second object in stockData should contain rel_likes property"
+        );
+        done();
+      });
+  });
 });
